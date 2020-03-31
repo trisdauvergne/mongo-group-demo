4179ce14e5fc0c5f25b0668b2b67d3cfd27fe768
trisdauvergne <tristan.dauvergne@gmail.com>
2020-03-31 09:26:10 +0000

    200331 - 11:26 initial commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+import os
+from os import path
+if path.exists("env.py"):
+    import env
+
+
+app = Flask(__name__)
+
+
+MONGO_URI = os.environ.get("MONGO_URI")
+
+
+@app.route("/")
+def home():
+    return render_template('hello.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',
+    port=5000,
+    debug=True)
---
+++ env.py
@@ -0,0 +1,3 @@
+import os
+
+os.environ["MONGO_URI"] = "mongodb+srv://trisdauvergnemongodb:mongofortristan1@task-manager-dehbo.mongodb.net/test_database?retryWrites=true&w=majority"
